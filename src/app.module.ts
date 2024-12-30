643765a28e3a93b0d22244ac85c8581094cfff94
Ruben Parra <rdparrac50@gmail.com>
2024-12-30 10:48:10 -0500

    adding environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/task-manager-db'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}.xz2gm.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`,
+    ),
+    // MongooseModule.forRoot('mongodb://127.0.0.1:27017/task-manager-db'),
     TasksModule,
   ],
 })
